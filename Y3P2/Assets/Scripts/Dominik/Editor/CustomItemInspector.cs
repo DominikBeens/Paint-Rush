3326a9a0ffb24bac485c2a28c3471b277ec85c41
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-20 16:54:30 +0100

    added custom item inspector to properly save scriptable objects

---
+++ Y3P2/Assets/Scripts/Dominik/Editor/CustomItemInspector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Item), true)]
+public class CustomItemInspector : Editor 
+{
+
+    public Item item;
+
+    private void OnEnable()
+    {
+        item = (Item)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(20);
+
+        #region Save Asset Button
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Save Asset"))
+        {
+            SaveAssetsDirty();
+        }
+
+        GUILayout.EndHorizontal();
+        #endregion
+    }
+
+    private void SaveAssetsDirty()
+    {
+        EditorUtility.SetDirty(target);
+        AssetDatabase.SaveAssets();
+    }
+}
