a56139ce1162d769d5215523a162858ed18e65da
gmd1apeterbos <97040824@st.deltion.nl>
2018-11-24 17:16:45 +0100

    Added JumpPad

---
+++ Y3P2/Assets/Scripts/Peter/JumpPad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour {
+
+    public float upForce = 10;
+    public float forwardForce = 2000;
+	
+    private void Launch(Rigidbody player)
+    {
+        player.AddRelativeForce(Vector3.forward * forwardForce, ForceMode.Impulse);
+        player.AddRelativeForce(Vector3.up * upForce, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.root.tag == "Player")
+        {
+            Launch(other.transform.root.GetComponent<Rigidbody>());
+        }
+    }
+
+    
+}
