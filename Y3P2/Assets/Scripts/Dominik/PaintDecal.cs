80401e43a1ee2b83b35498485d88fdd3bd0af4ae
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-30 16:39:25 +0100

    decals are based off of shadows now

--- Y3P2/Assets/Scripts/Dominik/PaintDecal.cs
+++ Y3P2/Assets/Scripts/Dominik/PaintDecal.cs
@@ -1,26 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PaintDecal : MonoBehaviour
 {
 
     private Projector projector;
-    private Material mat;
+
+    [SerializeField] private List<DecalData> decalData = new List<DecalData>();
+
+    [System.Serializable]
+    public struct DecalData
+    {
+        public Material decalMat;
+        public Color decalColor;
+    }
 
     private void Awake()
     {
         projector = GetComponentInChildren<Projector>();
-        mat = Instantiate(projector.material);
-        projector.material = mat;
     }
 
     public void Initialise(Color color)
     {
-        mat.color = color;
+        projector.material = GetDecalMat(color);
 
         RaycastHit hit;
         if (Physics.Raycast(projector.transform.position, projector.transform.forward, out hit, 0.3f))
         {
             transform.SetParent(hit.transform);
         }
+    }
+
+    private Material GetDecalMat(Color32 color)
+    {
+        for (int i = 0; i < decalData.Count; i++)
+        {
+            if (decalData[i].decalColor == color)
+            {
+                return decalData[i].decalMat;
+            }
+        }
+
+        Debug.LogWarning("Couldnt find decal material.");
+        return null;
     }
 }
