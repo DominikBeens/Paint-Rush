c9de5635a4e0d51ae3a6930f192c095196fd84d8
Dominik Beens <dominik.beens@hotmail.nl>
2018-11-30 16:46:32 +0100

    randomized decals, limited possible amount of decals in scene

--- Y3P2/Assets/Scripts/Dominik/PaintDecal.cs
+++ Y3P2/Assets/Scripts/Dominik/PaintDecal.cs
@@ -5,7 +5,9 @@ public class PaintDecal : MonoBehaviour
 {
 
     private Projector projector;
+    private float defaultSize;
 
+    [SerializeField] private float randomSizeRange = 0.25f;
     [SerializeField] private List<DecalData> decalData = new List<DecalData>();
 
     [System.Serializable]
@@ -18,11 +20,14 @@ public class PaintDecal : MonoBehaviour
     private void Awake()
     {
         projector = GetComponentInChildren<Projector>();
+        defaultSize = transform.localScale.x;
     }
 
     public void Initialise(Color color)
     {
         projector.material = GetDecalMat(color);
+        RandomizeSize();
+        RandomizeRotation();
 
         RaycastHit hit;
         if (Physics.Raycast(projector.transform.position, projector.transform.forward, out hit, 0.3f))
@@ -43,5 +48,16 @@ public class PaintDecal : MonoBehaviour
 
         Debug.LogWarning("Couldnt find decal material.");
         return null;
+    }
+
+    private void RandomizeSize()
+    {
+        float randomSize = Random.Range(defaultSize - randomSizeRange, defaultSize + randomSizeRange);
+        transform.localScale = new Vector3(randomSize, randomSize, randomSize);
+    }
+
+    private void RandomizeRotation()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Random.Range(0, 360));
     }
 }
