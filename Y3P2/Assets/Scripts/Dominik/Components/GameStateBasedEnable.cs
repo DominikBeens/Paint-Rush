c54d01b877d76e8607cdff47c3560038e12856b4
Dominik Beens <dominik.beens@hotmail.nl>
2018-12-16 20:24:48 +0100

    added arena cams in lobby

---
+++ Y3P2/Assets/Scripts/Dominik/Components/GameStateBasedEnable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateBasedEnable : MonoBehaviour
+{
+
+    [SerializeField] private List<Behaviour> componentsToToggle = new List<Behaviour>();
+    [SerializeField] private List<ToggleSetting> settings = new List<ToggleSetting>();
+
+    [System.Serializable]
+    private struct ToggleSetting
+    {
+        public GameManager.GameState state;
+        public bool toggle;
+    }
+
+    private void Awake()
+    {
+        GameManager.OnGameStateChanged += Toggle;
+    }
+
+    private void Toggle(GameManager.GameState newState)
+    {
+        for (int i = 0; i < settings.Count; i++)
+        {
+            if (settings[i].state == newState)
+            {
+                for (int ii = 0; ii < componentsToToggle.Count; ii++)
+                {
+                    componentsToToggle[ii].enabled = settings[i].toggle;
+                }
+
+                return;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnGameStateChanged -= Toggle;
+    }
+}
