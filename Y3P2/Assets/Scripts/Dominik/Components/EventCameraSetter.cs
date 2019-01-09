6064eec9e249638a8937672c1e2112bce9e34597
Dominik Beens <dominik.beens@hotmail.nl>
2019-01-09 12:48:16 +0100

    added EventCameraSetter to lots of world space UI

---
+++ Y3P2/Assets/Scripts/Dominik/Components/EventCameraSetter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EventCameraSetter : MonoBehaviour
+{
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        if (!cam)
+        {
+            cam = FindObjectOfType<Camera>();
+        }
+
+        if (cam)
+        {
+            GetComponent<Canvas>().worldCamera = cam;
+        }
+    }
+}
