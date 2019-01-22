2d2bfceddf36988966e2f1d62af0357a09c3d0ba
Dominik Beens <dominik.beens@hotmail.nl>
2019-01-22 13:13:20 +0100

    reduced mark capture duration, menu tweak

---
+++ Y3P2/Assets/Scripts/Dominik/Components/ButtonGoToURL.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonGoToURL : MonoBehaviour
+{
+
+    private Button button;
+
+    [SerializeField] private string url;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => GoToURL());
+    }
+
+    private void GoToURL()
+    {
+        if (!string.IsNullOrEmpty(url))
+        {
+            Application.OpenURL(url);
+        }
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(() => GoToURL());
+    }
+}
