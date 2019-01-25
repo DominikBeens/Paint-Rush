f8b417d0220bc65b407a8a7007cf7c5104e89d1b
gmd1apeterbos <97040824@st.deltion.nl>
2019-01-25 11:23:46 +0100

    RREEEEEE

---
+++ Y3P2/Assets/MAKEACTIVE.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAKEACTIVE : MonoBehaviour {
+
+    private bool boooool;
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameObject.activeSelf && !boooool)
+        {
+            gameObject.SetActive(true);
+            boooool = true;
+        }
+    }
+}
